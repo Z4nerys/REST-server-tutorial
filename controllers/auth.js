31ad874d8b053117f5d618920adebde5f867c37a
Gasty <gmfkratze@gmail.com>
2022-05-18 14:36:13 -0300

    jwt added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,54 @@
-const login = (req, res) =>{
-    res.json(req.body)
+const bcryptjs = require('bcryptjs');
+const { generarJWT } = require('../helpers/generar-jwt');
+const Usuario = require('../models/usuario');
+
+const login = async (req, res) => {
+
+    const { correo, password } = req.body
+
+    try {
+        //Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo })
+
+        if (!usuario){
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrect - correo'
+            })
+        }
+
+        //SI el usuario esta activo
+        if (!usuario.estado){
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrect - estado: false'
+            })
+        }
+
+        //Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrect - contraseña'
+            })
+        }
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
+
 }
 
 module.exports = {
     login
 }
+
+//ctrl + p para abrir archivos 
---
+++ helpers/generar-jwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const generarJWT = ( uid = '' ) => {
+    //no record information sensitive in jsonwebtoken
+    return new Promise( (resolve, reject ) =>{
+
+        const payload = { uid }
+
+        jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
+            expiresIn : '4h'
+        }, (err, token) => {
+            if( err ){
+                console.log(err);
+                reject('No se pudo generar el token');
+            }else{
+                resolve(token);
+            }
+        })
+
+
+    })
+}
+
+module.exports = {
+    generarJWT
+}
